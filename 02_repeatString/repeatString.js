e5d1bd7fe3e121a8de3d7720f40119ee9b7d3f38
Qomorudeen Mustopha <adewunmi99@gmail.com>
2022-10-13 15:24:32 +0100

    al tests passed for exercise 1 amd 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let number
+    if (number) {
+        return number
+    }
+    if (num < 0) {
+        return 'ERROR'
+    }
+    else
+    return string.repeat(num)
 };
 
 // Do not edit below this line
