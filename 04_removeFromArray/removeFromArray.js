503ad231f45ddaaa3111e008fe8878984e03cc34
Qomorudeen Mustopha <adewunmi99@gmail.com>
2022-10-13 17:25:49 +0100

    all test cases for eercise 3 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,31 @@
-const removeFromArray = function() {
-
+/*
+const removeFromArray = function(arr) {
+    // slices arguments from the 1st index up to the very last one
+    // and puts them into a new array "args"
+    args = Array.prototype.slice.call(arguments, 1)
+    for (i = 0; i < args.length; i++) {
+        if (arr.includes(args[i])) {
+            // removes the current argument frm the array "arr"
+            arr.splice(arr.indexOf(args[i]), 1)
+        }
+    }
+    return arr
 };
+*/
+
+
+
+/****************** ANOTHER APPROACH USING THE REST PARAMETER ****************/
+const removeFromArray = function(arr, ...myArgs) {
+    for (i = 0; i < myArgs.length; i++) {
+        if (arr.includes(myArgs[i])) {
+            arr.splice(arr.indexOf(myArgs[i]), 1)
+        }
+    }
+    return arr
+}
+
+console.log(removeFromArray([1, 2, 3, 4, "hey"], 3, 2))
 
 // Do not edit below this line
 module.exports = removeFromArray;
