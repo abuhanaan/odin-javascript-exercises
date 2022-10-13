3315ded21d437173c8c4d35c3632a6b5cfeaf027
Qomorudeen Mustopha <adewunmi99@gmail.com>
2022-10-13 21:24:01 +0100

    All test cases passed for exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(x) {
+  let celsius = (x - 32) * (5/9)
+  celsius = Math.round(celsius * 10) / 10
+  return celsius
 };
 
-const ctof = function() {
-
+const ctof = function(x) {
+  let fahrenheit = (x * (9/5)) + 32
+  fahrenheit = Math.round(fahrenheit * 10) / 10
+  return fahrenheit
 };
 
 // Do not edit below this line
