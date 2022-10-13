b5124bfd0c514e8920682d000847b9deb3f440ed
Qomorudeen Mustopha <adewunmi99@gmail.com>
2022-10-13 20:42:47 +0100

    All test cases passed for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(param1, param2) {
+    if (param1 < 0 || param2 < 0) {
+        return "ERROR"
+    }
+    if (typeof(param1) != 'number' || typeof(param2) != 'number') {
+        return "ERROR"
+    }
+    let minParam = Math.min(param1, param2)
+    let maxParam = Math.max(param1, param2)
+    let result = 0
+    for (i = minParam; i <= maxParam; i++) {
+        result += i
+    }
+    return result
 };
+
+console.log(sumAll(123, 1))
 
 // Do not edit below this line
 module.exports = sumAll;
