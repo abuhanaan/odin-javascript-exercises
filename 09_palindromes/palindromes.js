f91c1c4329cbd2225ab1a23ff5f74e0f39931c19
Qomorudeen Mustopha <adewunmi99@gmail.com>
2022-10-24 12:07:04 +0100

    all test cases passed for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newArr = []
+    for (i = 0; i < str.length; i++) {
+        if (str[i].match(/[a-z]/i) != null) {
+            newArr.push(str[i])
+        }
+    }
+    let strArr = newArr.join('').toLowerCase()
+    let reversedStr = strArr.split('').reverse().join('')
+    // (strArr == reversedStr) ? true : false
+    if (strArr == reversedStr) {
+        return true
+    } else {
+        return false
+    }
 };
+
+console.log(palindromes('ZZZZ car, a man, a maracaz.'))
 
 // Do not edit below this line
 module.exports = palindromes;
