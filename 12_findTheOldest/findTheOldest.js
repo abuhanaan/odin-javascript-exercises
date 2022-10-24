1b00cd926cf355213be4cad9a875ca3f59477de1
Qomorudeen Mustopha <adewunmi99@gmail.com>
2022-10-24 17:45:24 +0100

    All test cases passed for findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function getAge(yearOfBirth, yearOfDeath) {
+        let age
+        const currentYear = new Date().getFullYear()
+        if (!yearOfDeath) {
+            age = currentYear - yearOfBirth
+        } else {
+            age = yearOfDeath - yearOfBirth
+        }
+        return age
+    }
 
+    return people.reduce((currPerson, nextPerson) => {
+        const currPersonAge = getAge(currPerson.yearOfBirth, currPerson.yearOfDeath)
+        const nextPersonAge = getAge(nextPerson.yearOfBirth, nextPerson.yearOfDeath)
+        console.log(`${currPerson.name} is ${currPersonAge} years while ${nextPerson.name} is ${nextPersonAge} years`)
+        return currPersonAge > nextPersonAge ? currPerson : nextPerson
+    })
+    
 };
 
 // Do not edit below this line
